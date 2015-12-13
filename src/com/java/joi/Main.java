7859f7b9b861a224403af32f54b7e77cdfb2f778
mi-24v <yuusukessid@gmail.com>
2015-12-13 21:38:50 +0900

    よせんやったよ～(死亡)

--- src/com/java/joi/Main.java
+++ src/com/java/joi/Main.java
@@ -1,13 +1,113 @@
 package com.java.joi;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.stream.Collectors;
 
 public class Main{
+	private static ArrayList<Integer> Opecounts;
+	private static ArrayList<ArrayList<String>> OldFlag;
+	private static LinkedHashMap<ArrayList<Integer>,Integer> alexe = new LinkedHashMap<>();
+	private static int Width,Height;
 	public static void main(String[] args){
 		GeneralIO g = new GeneralIO();
 		g.interactive();
 		ArrayList<ArrayList<String>> rawdata = g.getInputdata();
 		rawdata.forEach(a -> {a.forEach(s -> System.out.print(s));System.out.println();});
-
+		Width=Integer.parseInt(rawdata.get(0).get(1));
+		Height =Integer.parseInt(rawdata.get(0).get(0));
+		rawdata.remove(0);
+		OldFlag = rawdata;
+		Opecounts = new ArrayList<>();
+		Opecounts.add(nurikae(1,1,Height-2,Integer.MAX_VALUE));
+		Collections.sort(Opecounts);
+		int minOpe = Opecounts.get(0);
+		ArrayList<ArrayList<String>> ans = new ArrayList<>();
+		ans.add(new ArrayList<String>(){{add(Integer.toString(minOpe));}});
+		g.setOutputdata(ans);
+		g.output();
 	}
+
+	private static int nurikae(int Wcount,int Bcount,int Rcount,int minope){
+		System.out.println("minope:"+minope);
+		ArrayList<Integer> exe = new ArrayList<Integer>(){{add(Wcount);add(Bcount);add(Rcount);}};
+		System.out.println("args:"+exe.toString());
+		if(Wcount + Bcount + Rcount > Height && Wcount ==Bcount && Bcount == Rcount){
+			System.out.println("[returned]");
+			return minope;
+		}else if(Rcount == 0){
+			nurikae(1,Height-2,1,minope);
+		}else if(Bcount == 0){
+			nurikae(Height-2,1,1,minope);
+		}else if(Wcount == 0){
+			nurikae(2,2,1,minope);
+		}else if(Wcount + Bcount > Height -Rcount && Bcount == Wcount){
+			nurikae(1,2,2,minope);
+		}else if(Bcount + Rcount > Height -Wcount && Bcount == Rcount){
+			nurikae(2,1,2,minope);
+		}else if(Wcount + Rcount > Height -Bcount && Wcount == Rcount){
+			nurikae(1,1,1,minope);
+		}
+		int ope = Integer.MAX_VALUE;
+		if(alexe.containsKey(exe)){
+			System.out.println("[same thing]");
+			ope = Math.min(minope,alexe.get(exe));
+		}else if(Wcount < Height && Bcount <Height && Rcount < Height){
+			ope = 0;
+			for(int i=0;i<Wcount;i++){
+				ArrayList<String> line = OldFlag.get(i);
+				ope += nurikae(line,"W");
+			}
+			for(int i=Wcount;i<Bcount+Wcount;i++){
+				ArrayList<String> line = OldFlag.get(i);
+				ope += nurikae(line,"B");
+			}
+			for(int i=Bcount+Wcount;i<Height;i++){
+				ArrayList<String> line = OldFlag.get(i);
+				ope += nurikae(line,"R");
+			}
+			System.out.println("ope:"+ope);
+			alexe.put(exe, ope);
+			ope = Math.min(ope, minope);
+		}else{
+			System.err.println("over!");
+			if(Wcount >= Height && Bcount >= Height && Rcount >= Height){
+				System.out.println("[returned]");
+				return minope;
+			}else if(Wcount >= Height){
+				return nurikae(1,Bcount,Rcount,minope);
+			}else if(Bcount >= Height){
+				return nurikae(Wcount,1,Rcount,minope);
+			}else if(Rcount >= Height){
+				return nurikae(Wcount,Bcount,1,minope);
+			}else{
+				System.err.println("returned");
+				return Math.min(minope, ope);
+			}
+		}
+		if(Wcount == 1 && Bcount == 1&& Rcount ==1){
+			return nurikae(Wcount+1,Bcount+1,Rcount+1,ope);
+		}else if(Wcount == Bcount && Rcount == 1){
+			return nurikae(Wcount+1,Bcount+1,Rcount,ope);
+		}else if(Wcount == 1){
+			return nurikae(Wcount,Bcount+1,Rcount-1,ope);
+		}else if(Rcount == 1){
+			return nurikae(Wcount+1,Bcount-1,Rcount,ope);
+		}else if(Bcount == 1){
+			return nurikae(Wcount-1,Bcount,Rcount+1,ope);
+		}else if(Bcount == Rcount || Wcount == 1){
+			return nurikae(Wcount,Bcount+1,Rcount+1,ope);
+		}else if(Wcount == Rcount || Bcount == 1){
+			return nurikae(Wcount+1,Bcount,Rcount+1,ope);
+		}else{
+			System.err.println("returned");
+			return Math.min(minope, ope);
+		}
+	}
+
+	private static int nurikae(ArrayList<String> line, String s){
+		return (int) (Width - line.stream().filter(str -> str.equals(s)).collect(Collectors.toList()).size());
+	}
+
 }
--- src/com/java/joiArchive/JoiArchive.java
+++ src/com/java/joiArchive/JoiArchive.java
@@ -1,12 +1,15 @@
 package com.java.joiArchive;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.stream.Collectors;
 
 import com.java.joi.GeneralIO;
 
 public class JoiArchive {
 
 	//「和と差」のもんだい(練習ページとい1)
+	@SuppressWarnings("serial")
 	public void AdditionSubtraction(){
 		GeneralIO g = new GeneralIO();
 		g.interactive();
@@ -20,6 +23,147 @@ public class JoiArchive {
 				add(Integer.toString(Integer.parseInt(rawdata.get(0).get(0))-Integer.parseInt(rawdata.get(0).get(1))));
 			}});
 		}};
+		g.setOutputdata(ans);
+		g.output();
+	}
+
+	//「水道料金」2014年とい1
+	@SuppressWarnings("serial")
+	public void WaterRate(){
+		GeneralIO g = new GeneralIO();
+		g.interactive();
+		ArrayList<ArrayList<String>> rawdata = g.getInputdata();
+		rawdata.forEach(a -> {a.forEach(s -> System.out.print(s));System.out.println();});
+		final int XperYen = Integer.parseInt(rawdata.get(0).get(0));
+		final int YconstYen = Integer.parseInt(rawdata.get(1).get(0));
+		final int YmaxWater = Integer.parseInt(rawdata.get(2).get(0));
+		final int YperYen = Integer.parseInt(rawdata.get(3).get(0));
+		final int TotalWater = Integer.parseInt(rawdata.get(4).get(0));
+		final int ans;
+		if(TotalWater > YmaxWater){
+			if(YperYen > XperYen && XperYen < YconstYen){
+				ans = XperYen * TotalWater;
+			}else{
+				ans = YconstYen + YperYen * (TotalWater - YmaxWater);
+			}
+		}else{
+			if(XperYen > YconstYen){
+				ans = YconstYen;
+			}else if(XperYen > YperYen){
+				ans = YconstYen + YperYen * (TotalWater - YmaxWater);
+			}else{
+				ans = YconstYen + XperYen * (TotalWater - YmaxWater);
+			}
+		}
+		g.setOutputdata(new ArrayList<ArrayList<String>>(){{
+			add(new ArrayList<String>(){{add(Integer.toString(ans));}});
+		}});
+		g.output();
+	}
+
+	//「クリスマスパーティー」2014とい2
+	@SuppressWarnings("serial")
+	public void ChristmasParty(){
+		GeneralIO g = new GeneralIO();
+		g.interactive();
+		ArrayList<ArrayList<String>> rawdata = g.getInputdata();
+		rawdata.forEach(a -> {a.forEach(s -> System.out.print(s));System.out.println();});
+		final int friends = Integer.parseInt(rawdata.get(0).get(0));
+		final int games = Integer.parseInt(rawdata.get(1).get(0));
+		final ArrayList<Integer> targets = rawdata.get(2).stream().map(s -> Integer.parseInt(s)).collect(Collectors.toCollection(ArrayList<Integer>::new));
+		ArrayList<ArrayList<Integer>> gamedata = new ArrayList<>();
+		ArrayList<Integer> result = new ArrayList<>(friends);
+		for(int i = 0; i<friends; i++){result.add(0);}
+		for (int i = 3; i < games+3; i++) {
+			gamedata.add(rawdata.get(i).stream().map(s -> Integer.parseInt(s)).collect(Collectors.toCollection(ArrayList<Integer>::new)));
+		}
+		System.out.println("targets:"+targets);
+		for(int i = 0; i < games; i++){
+			final int target = targets.get(i);
+			int miss = 0;
+			ArrayList<Integer> data = gamedata.get(i);
+			System.out.println("data:"+data.toString()+" target:"+target);
+			for (int j = 0; j < data.size(); j++) {
+				if(data.get(j) == target){
+					System.out.println("match:"+result.get(j));
+					result.set(j, result.get(j) + 1);
+					System.out.println("->"+result.get(j));
+				}else{
+					miss++;
+				}
+			}
+			result.set(target-1, result.get(target-1) + miss);
+		}
+		ArrayList<ArrayList<String>> ans = new ArrayList<>();
+		result.forEach(r -> ans.add(new ArrayList<String>(){{add(Integer.toString(r));}}));
+		g.setOutputdata(ans);
+		g.output();
+	}
+
+	//2015よせん1もんめ
+	@SuppressWarnings("serial")
+	public void SelectSubjects(){
+		GeneralIO g = new GeneralIO();
+		g.interactive();
+		ArrayList<ArrayList<String>> rawdata = g.getInputdata();
+		rawdata.forEach(a -> {a.forEach(s -> System.out.print(s));System.out.println();});
+		ArrayList<Integer> science = new ArrayList<Integer>(){{
+			add(Integer.parseInt(rawdata.get(0).get(0)));
+			add(Integer.parseInt(rawdata.get(1).get(0)));
+			add(Integer.parseInt(rawdata.get(2).get(0)));
+			add(Integer.parseInt(rawdata.get(3).get(0)));
+		}};
+		ArrayList<Integer> social = new ArrayList<Integer>(){{
+			add(Integer.parseInt(rawdata.get(4).get(0)));
+			add(Integer.parseInt(rawdata.get(5).get(0)));
+		}};
+		Collections.sort(science);
+		Collections.sort(social);
+		int sciencesum=0,socialsum=0;
+		for(int i = science.size()-1;i>0;i--){
+			sciencesum += science.get(i);
+		}
+		for(int i = social.size()-1;i>0;i--){
+			socialsum += social.get(i);
+		}
+		final String ansstr = Integer.toString(sciencesum+socialsum);
+		ArrayList<ArrayList<String>> ans = new ArrayList<>();
+		ans.add(new ArrayList<String>(){{add(ansstr);}});
+		g.setOutputdata(ans);
+		g.output();
+	}
+
+	//2015よせん2もんめ
+	@SuppressWarnings("serial")
+	public void SwappingBips(){
+		GeneralIO g = new GeneralIO();
+		g.interactive();
+		ArrayList<ArrayList<String>> rawdata = g.getInputdata();
+		rawdata.forEach(a -> {a.forEach(s -> System.out.print(s));System.out.println();});
+		final int students = Integer.parseInt(rawdata.get(0).get(0));
+		final int totalbaton = Integer.parseInt(rawdata.get(0).get(1));
+		ArrayList<Integer> zekkens = new ArrayList<>();
+		for(int i = 1; i <= students; i++){
+			zekkens.add(Integer.parseInt(rawdata.get(i).get(0)));
+		}
+		for(int i=1;i<=totalbaton;i++){
+			for(int j=0;j<zekkens.size();j++){
+				if(j < zekkens.size()-1){
+					int amari1 = i % zekkens.get(j),
+						amari2 = i % zekkens.get(j+1);
+					if(amari1 > amari2){
+						int tmp = zekkens.get(j);
+						zekkens.set(j, zekkens.get(j+1));
+						zekkens.set(j+1, tmp);
+					}
+				}
+			}
+		}
+		ArrayList<ArrayList<String>> ans = new ArrayList<>();
+		for(int i=0;i<zekkens.size();i++){
+			final int zekken = zekkens.get(i);
+			ans.add(new ArrayList<String>(){{add(Integer.toString(zekken));}});
+		}
 		g.setOutputdata(ans);
 		g.output();
 	}
